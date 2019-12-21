d03b990eebe90ee786bd536a4435e1772905f3e3
Fernando Paredes <nano@fdp.io>
2019-12-21 13:15:05 -0500

    ✨ Add Collection.stride

--- Sources/Nano/Swift/Extensions/Collection+.swift
+++ Sources/Nano/Swift/Extensions/Collection+.swift
@@ -58,3 +58,66 @@ public extension Collection {
         return (a1, a2)
     }
 }
+
+// MARK: - Strideable Index
+
+public extension Collection where Index: Strideable {
+    /// Returns a lazily evaluated Sequence, yielding at the desired `step`.
+    /// - Parameter step: The number of elements to offset by.
+    func stride(by step: Index.Stride) -> UnfoldSequence<Element, Index> {
+        stride(by: step, from: startIndex, to: endIndex)
+    }
+
+    /// Returns a lazily evaluated Sequence, yielding at the desired `step` starting from the given index.
+    /// - Parameter step: The number of elements to offset by.
+    /// - Parameter start: The starting index.
+    func stride(by step: Index.Stride, from start: Index) -> UnfoldSequence<Element, Index> {
+        stride(by: step, from: start, to: endIndex)
+    }
+
+    /// Returns a lazily evaluated Sequence, yielding at the desired `step` ending at the given end index.
+    /// - Parameter step: The number of elements to offset by.
+    /// - Parameter end: The ending index.
+    func stride(by step: Index.Stride, to end: Index) -> UnfoldSequence<Element, Index> {
+        stride(by: step, from: startIndex, to: end)
+    }
+
+    /// Returns a lazily evaluated Sequence, yielding at the desired `step` starting from the given index and ending at the given end index.
+    /// - Parameter step: The number of elements to offset by.
+    /// - Parameter start: The starting index.
+    /// - Parameter end: The ending index.
+    func stride(by step: Index.Stride, from start: Index, to end: Index) -> UnfoldSequence<Element, Index> {
+        sequence(state: start, next: { state in
+            guard state < end else { return nil }
+            defer { state = state.advanced(by: step) }
+            return self[state]
+        })
+    }
+
+    /// Returns a lazily evaluated Sequence, yielding at the desired `step` through a given end index.
+    /// - Parameter step: The number of elements to offset by.
+    /// - Parameter end: The ending index.
+    func stride(by step: Index.Stride, through end: Index) -> UnfoldSequence<Element, Index> {
+        stride(by: step, from: startIndex, through: end)
+    }
+
+    /// Returns a lazily evaluated Sequence, yielding at the desired `step` starting from the given index and through the given end index.
+    /// - Parameters:
+    ///   - step: The number of elements to offset by.
+    ///   - start: The starting index.
+    ///   - end: The ending index.
+    func stride(by step: Index.Stride, from start: Index, through end: Index) -> UnfoldSequence<Element, Index> {
+        guard !isEmpty else {
+            return sequence(state: start, next: { _ in nil })
+        }
+
+        return sequence(state: start, next: { state in
+            guard state <= end else {
+                return nil
+            }
+
+            defer { state = state.advanced(by: step) }
+            return self[state]
+        })
+    }
+}
--- Tests/NanoTests/Swift/Extensions/CollectionTests.swift
+++ Tests/NanoTests/Swift/Extensions/CollectionTests.swift
@@ -28,6 +28,52 @@ final class CollectionTests: XCTestCase {
         XCTAssertEqual(splitWizards.1, [wizards[0]])
     }
 
+    func test_strideByStep() {
+        let numbers = Array(1...10)
+        var oddNumberSequence = numbers.stride(by: 2)
+        XCTAssertEqual(oddNumberSequence.next(), 1)
+        XCTAssertEqual(oddNumberSequence.next(), 3)
+        XCTAssertEqual(oddNumberSequence.next(), 5)
+        XCTAssertEqual(oddNumberSequence.next(), 7)
+        XCTAssertEqual(oddNumberSequence.next(), 9)
+        XCTAssertNil(oddNumberSequence.next())
+    }
+
+    func test_strideByStep_fromStart() {
+        let numbers = Array(1...10)
+        var evenNumberSequence = numbers.stride(by: 2, from: 1)
+        XCTAssertEqual(evenNumberSequence.next(), 2)
+        XCTAssertEqual(evenNumberSequence.next(), 4)
+        XCTAssertEqual(evenNumberSequence.next(), 6)
+        XCTAssertEqual(evenNumberSequence.next(), 8)
+        XCTAssertEqual(evenNumberSequence.next(), 10)
+        XCTAssertNil(evenNumberSequence.next())
+    }
+
+    func test_strideByStep_toEnd() {
+        let numbers = Array(1...10)
+        var oddNumbersUpToFiveSequence = numbers.stride(by: 2, to: 5)
+        XCTAssertEqual(oddNumbersUpToFiveSequence.next(), 1)
+        XCTAssertEqual(oddNumbersUpToFiveSequence.next(), 3)
+        XCTAssertEqual(oddNumbersUpToFiveSequence.next(), 5)
+        XCTAssertNil(oddNumbersUpToFiveSequence.next())
+    }
+
+    func test_strideByStep_throughEnd() {
+        let numbers = Array(1...10)
+        var oddNumbersUpToFiveSequence = numbers.stride(by: 2, through: 5)
+        XCTAssertEqual(oddNumbersUpToFiveSequence.next(), 1)
+        XCTAssertEqual(oddNumbersUpToFiveSequence.next(), 3)
+        XCTAssertEqual(oddNumbersUpToFiveSequence.next(), 5)
+        XCTAssertNil(oddNumbersUpToFiveSequence.next())
+    }
+
+    func test_strideByStep_fromStart_throughEnd() {
+        let empty = [Int]()
+        var emptySequence = empty.stride(by: 2, from: 0, through: 10)
+        XCTAssertNil(emptySequence.next())
+    }
+
     func test_subscript_safe() {
         let numbers = [1, 2]
 
@@ -39,6 +85,11 @@ final class CollectionTests: XCTestCase {
         ("test_anySatisfy", test_anySatisfy),
         ("test_countWhere", test_countWhere),
         ("test_partitionBy", test_partitionBy),
+        ("test_strideByStep", test_strideByStep),
+        ("test_strideByStep_fromStart", test_strideByStep_fromStart),
+        ("test_strideByStep_toEnd", test_strideByStep_toEnd),
+        ("test_strideByStep_throughEnd", test_strideByStep_throughEnd),
+        ("test_strideByStep_fromStart_throughEnd", test_strideByStep_fromStart_throughEnd),
         ("test_subscript_safe", test_countWhere),
     ]
 }
