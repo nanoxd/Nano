a1310f429c9e3ecf93a5cfadde86cc91e855e298
Fernando Paredes <nano@fdp.io>
2020-07-18 19:34:01 -0400

    [SwiftUI] Add EnumPicker
    Adds a picker that can be instantiated from an enum

---
+++ Sources/Nano/SwiftUI/Views/EnumPicker.swift
@@ -0,0 +1,31 @@
+import Foundation
+import SwiftUI
+
+/// Creates a picker from an enumeration.
+///
+/// ```swift
+/// enum State: String, CaseIterable {
+///     case CA, NY, WA
+/// }
+///
+/// struct ContentView: View {
+///     @State private var state = State.CA
+///
+///     var body: some View {
+///         EnumPicker(selected: $state)
+///     }
+/// }
+/// ```
+@available(iOS 13.0, tvOS 13.0, OSX 10.15, *)
+struct EnumPicker<Element: Hashable & RawRepresentable & CaseIterable>: View where Element.RawValue == String {
+    @Binding var selected: Element
+    var title = ""
+
+    var body: some View {
+        Picker(selection: $selected, label: Text(title)) {
+            ForEach(Array(Element.allCases), id: \.rawValue) {
+                Text($0.rawValue).tag($0)
+            }
+        }
+    }
+}
