d686b688ace1c800f84512082de40f53bcc1d20b
Fernando Paredes <nano@fdp.io>
2021-12-09 20:17:32 -0600

    Set minimum versions to iOS/tvOS 14 / macOS 11

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "Nano",
     platforms: [
-        .macOS(.v10_15),
-        .iOS(.v13),
-        .tvOS(.v13),
+        .macOS(.v11),
+        .iOS(.v14),
+        .tvOS(.v14),
     ],
     products: [
         .library(
